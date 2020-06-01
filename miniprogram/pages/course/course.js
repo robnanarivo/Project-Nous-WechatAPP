7f321a83d67f400946829b3fda094f61f06f1deb
robnanarivo <58379780+robnanarivo@users.noreply.github.com>
2020-06-01 23:41:51 +0800

    Disable selection for courses which have enrolled more than 15 students

--- miniprogram/pages/course/course.js
+++ miniprogram/pages/course/course.js
@@ -8,9 +8,17 @@ Page({
     courseInfoAM: [], //上午可选的课
     courseInfoPM: [], //下午可选的课
     courseSelected: {
-      AM: [],
-      PM: [],
-    }, //已选课程，数组第一个值是课程名字，第二个是课程ID
+      AM: {
+        courseName: null,
+        courseID: null,
+        _id: null,
+      },
+      PM: {
+        courseName: null,
+        courseID: null,
+        _id: null,
+      },
+    }, //已选课程，数组第一个值是课程名字，第二个是课程ID，第三个是数据
 
     // student info
     venue: null, //读书营地点
@@ -61,10 +69,10 @@ Page({
         success: function(res) {
           page.setData({
             courseInfoAM: res.data.filter(obj => {
-              return obj.time === "AM"
+              return (obj.time === "AM" && obj.enrolledStudents.length < 15)
             }),
             courseInfoPM: res.data.filter(obj => {
-              return obj.time === "PM"
+              return (obj.time === "PM" && obj.enrolledStudents.length < 15)
             }),
             loading: false,
           });
@@ -82,12 +90,20 @@ Page({
 
     if (this.data.finishedAM === false) {
       this.setData({
-        "courseSelected.AM": selected,
+        "courseSelected.AM": {
+          courseName: selected[0],
+          courseID: selected[1],
+          _id: selected[2],
+        },
         isDisabledNext: false,
       });
     } else {
       this.setData({
-        "courseSelected.PM": selected,
+        "courseSelected.PM": {
+          courseName: selected[0],
+          courseID: selected[1],
+          _id: selected[2],
+        },
         isDisabledSubmit: false,
       });
     }
@@ -114,29 +130,59 @@ Page({
 
   tapDialogConfirm(e) {
     if (e.detail.index === 1) {
-    const courseSelection = wx.cloud.database().collection("courseSelection");
-    let page = this;
+      const db = wx.cloud.database();
+      const courseSelection = db.collection("courseSelection");
+      const courseList = db.collection("courseList");
+      const _ = db.command;
+      let page = this;
 
-    courseSelection.add({
-      data: {
-        studentName: this.data.studentName,
-        courseSelected: {
-          AM: this.data.courseSelected.AM[1],
-          PM: this.data.courseSelected.PM[1],
+      courseList.doc(this.data.courseSelected.AM._id).update({
+        data: {
+          enrolledStudents: _.push(page.data.studentName)
         },
-      },
-      // TODO: 跳转确认page
-      success: function(res) {
-        page.setData({
-          submitSuccess: true,
-        });
-        console.log(res);
-      }
-    });
+        success: function(res) {
+          console.log(res.data)
+        }
+      });
+
+      courseList.doc(this.data.courseSelected.PM._id).update({
+        data: {
+          enrolledStudents: _.push(page.data.studentName)
+        },
+        success: function(res) {
+          console.log(res.data)
+        }
+      });
+
+      addCourseSelection(page, courseSelection);
     } else {
       this.setData({
         isSubmitting: false,
       });
     }
+
+    function addCourseSelection(page, courseSelection) {
+      courseSelection.add({
+        data: {
+          studentName: page.data.studentName,
+          courseSelected: {
+            AM: page.data.courseSelected.AM,
+            PM: page.data.courseSelected.PM,
+          },
+        },
+        success: function(res) {
+          page.setData({
+            submitSuccess: true,
+          });
+          console.log(res);
+        }
+      })
+    }
+  },
+
+  tapReturnToStatus() {
+    wx.navigateTo({
+      url: '../status/status',
+    })
   },
 })
