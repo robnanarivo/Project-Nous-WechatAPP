3f655034c950ccfd2396fa2b19e9a0cff832f3a6
robnanarivo <58379780+robnanarivo@users.noreply.github.com>
2020-06-12 10:31:02 +0800

    Page structure adjustment

--- miniprogram/pages/addCourse/addCourse.js
+++ miniprogram/pages/addCourse/addCourse.js
@@ -10,7 +10,11 @@ Page({
     courseList.add({
       data: toUpload,
       success: function(res) {
-        console.log(res);
+        wx.showToast({
+          title: "提交成功",
+          icon: "success",
+          duration: 2000,
+        });
       }
     })
     console.log('form发生了submit事件，携带数据为：', e.detail.value);
